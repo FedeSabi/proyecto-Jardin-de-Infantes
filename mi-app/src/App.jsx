0c545ec494074e86c56bfa47446bc21478f51574
Federico Sabiche <fsabiche>
2023-08-24 19:07:57 -0300

    creacion del Navbar

--- mi-app/src/App.jsx
+++ mi-app/src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
-import { Navbar } from './components/Navbar/navbar';
+import { Navbar } from './components/Navbar/Navbar';
 import { Jugamos } from './components/Jugamos/Jugamos';
 import { Aulas } from "./components/Aulas/Aulas";
 import { Instalaciones } from './components/Instalaciones/Instalaciones';
 import { Footer } from './components/Footer/Footer';
+
 function App() {
 
 
--- mi-app/src/components/Navbar/Navbar.jsx
+++ mi-app/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,13 @@ import './Navbar.css'
 
 const Navbar = () => {
     return(
-        <div>Navbar</div>
+        <div>
+            <a href="#">Nosotros</a>
+            <a href="#">Nuestros Docentes</a>
+            <a href="#">Instalaciones</a>
+            <a href="#">Contacto</a>
+            <a href="#">Redes Sociales</a>
+        </div>
     )
 }
 
