5d7867d94a0ef88af7cfc8d84fbccfce17af359f
Federico Sabiche <fsabiche>
2023-08-24 19:30:17 -0300

    titulo

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -6,11 +6,17 @@ import './Navbar.css'
 const Navbar = () => {
     return(
         <div>
+            <div>
+            <h1>Jardin Nubes de Algodon</h1>
+            </div>
+            <div>
             <a href="#">Nosotros</a>
             <a href="#">Nuestros Docentes</a>
             <a href="#">Instalaciones</a>
             <a href="#">Contacto</a>
             <a href="#">Redes Sociales</a>
+            </div>
+           
         </div>
     )
 }
