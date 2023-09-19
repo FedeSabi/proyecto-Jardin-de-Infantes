dc1b8887cde2622a7ef1893a4ac5514c3e551fe5
miguel evangelista <miguelbranagan30@gmail.com>
2023-09-19 00:19:22 -0400

    fixing ratings and footer componets

--- Front/src/components/Footer.jsx
+++ Front/src/components/Footer.jsx
@@ -1,5 +1,145 @@
 const Footer = () => {
-    return <div>Footer</div>;
+    const currentYear = new Date().getFullYear(); // Get the current year
+
+    return (
+        <footer className="px-4 pb-4 xl:px-0 xl:pb-[80px]">
+            <div className=" hidden xl:block h-[1px] w-full bg-[#EEEEEE] my-6"></div>
+
+            <div className=" xl:flex xl:gap-[168px] xl:py-[80px] max-w-screen-xl mx-auto">
+                <div className=" xl:w-[353px]">
+                    <h2 className=" text-2xl font-bold leading-8 mr-[202px] xl:mr-0">
+                        Nubes de algodón
+                    </h2>
+                    <p className=" text-lg leading-8 mt-4 mr-[45px] xl:mr-0">
+                        Quisque egestas ipsum vitae erat faucibus, non volutpat
+                        arcu faucibus. Phasellus auctor sed ligula.
+                    </p>
+                </div>
+
+                <div className=" xl:flex xl:gap-[88px]">
+                    <div className="line"></div>
+
+                    <div>
+                        <h3 className=" mb-4 font-medium tracking-[0.08px]">
+                            Acerca de Nosotros
+                        </h3>
+                        <ul>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    ¿Quiénes somos?
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Precios
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Blog
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Aulas
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div>
+                        <h3 className=" mb-4 font-medium tracking-[0.08px]">
+                            Servicios
+                        </h3>
+                        <ul>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Cunas
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Guardería
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Jardín
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Escuela de Padres
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Taller
+                                </a>
+                            </li>
+                            <li>
+                                <a className=" footerLinks" href="#">
+                                    Cocina
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div>
+                        <h3 className=" mb-4 font-medium tracking-[0.08px]">
+                            Síguenos
+                        </h3>
+                        <ul className=" flex gap-x-3 mt-4">
+                            <li className="bg-black text-white rounded-full">
+                                <a
+                                    href="#"
+                                    className="h-[2rem] w-[2rem] flex items-center justify-center">
+                                    <i className="fa-brands fa-instagram"></i>
+                                </a>
+                            </li>
+
+                            <li className="bg-black text-white rounded-full">
+                                <a
+                                    href="#"
+                                    className="h-[2rem] w-[2rem] flex items-center justify-center">
+                                    <i className="fa-brands fa-facebook-f"></i>
+                                </a>
+                            </li>
+
+                            <li className="bg-black text-white rounded-full">
+                                <a
+                                    href="#"
+                                    className="h-[2rem] w-[2rem] flex items-center justify-center">
+                                    <i className="fa-brands fa-linkedin-in"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="line"></div>
+
+            <div className="w-full max-w-screen-xl mx-auto xl:flex xl:items-center xl:justify-between xl:mt-[100px]">
+                <span className="text-[#7C858E] sm:text-center">
+                    Copyright © {currentYear} Kids by johnnathandesigner. All
+                    Rights Reserved.
+                </span>
+                <ul className="flex justify-between xl:gap-[52px] items-center text-[#7C858E] mt-[40px] xl:mt-0">
+                    <li>
+                        <a href="#">Aviso legal</a>
+                    </li>
+                    <li>
+                        <a href="#">Política de Privacidad</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
 };
 
 export { Footer };
---
+++ Front/src/components/Ratings.jsx
@@ -0,0 +1,59 @@
+export const Ratings = () => {
+    return (
+        <section className=" px-4 xl:px-0 pt-[80px] pb-[232px] xl:pb-[54px] mb-[184px] bg-[#F5F5F5] xl:text-center">
+            <div className=" max-w-screen-lg mx-auto">
+                <h2 className=" text-[32px] font-semibold leading-[42px] tracking-[0.075px] text-center xl:mx-[260px]">
+                    Lo que están diciendo los papás acerca de Nosotros
+                </h2>
+                <p className=" mt-8 text-lg leading-[32px] tracking-[0.09px] xl:mx-8">
+                    Quisque egestas ipsum vitae erat faucibus, non volutpat arcu
+                    faucibus. Phasellus auctor sed ligula in bibendum. Maecenas
+                    vel ultrices nulla. Aliquam nunc nulla, consectetur quis
+                    varius eget, tempor a nisi. Integer hendrerit viverra enim,
+                    in mattis purus fermentum et. Quisque sit amet enim a arcu
+                    euismod dapibus eget sit amet est.
+                </p>
+            </div>
+
+            <div className=" text-center xl:mt-[136px]">
+                <div className=" mb-[18px]">
+                    <p className=" text-lg font-semibold leading-[32px] tracking-[0.09px]">
+                        Noah Emma
+                    </p>
+                    <p className=" text-sm leading-4">Madre de henry</p>
+                </div>
+
+                <ul className=" flex items-center justify-center gap-2">
+                    <li>
+                        <i className="fa-solid fa-star text-[#7C858E]"></i>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-star text-[#7C858E]"></i>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-star text-[#7C858E]"></i>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-star text-[#7C858E]"></i>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-star text-[#7C858E]"></i>
+                    </li>
+                </ul>
+            </div>
+
+            <div className=" relative max-w-screen-lg mx-auto">
+                <img
+                    className="hidden xl:block absolute right-[100px] bottom-[40px] z-10"
+                    src="/ArrowNoah.svg"
+                    alt="ArrowNoah"
+                />
+                <img
+                    className="absolute left-1/2 transform -translate-x-1/2 bottom-50 xl:-bottom-[150px] lg:left-auto lg:right-0 xl:-translate-x-[10px]"
+                    src="/Rectangle 60.svg"
+                    alt="Rectangle"
+                />
+            </div>
+        </section>
+    );
+};
