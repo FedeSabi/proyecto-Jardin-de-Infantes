d6e242e5df465d0b77612ea2c3afbdf547f6ac54
miguel <miguelbranagan30@gmail.com>
2023-10-30 20:42:11 -0400

    facilities header background

--- Front/src/components/FacilitiesComponents/FacilitiesHeader.jsx
+++ Front/src/components/FacilitiesComponents/FacilitiesHeader.jsx
@@ -1,7 +1,7 @@
 export const FacilitiesHeader = () => {
   return (
-    <header className=" h-[367px] bg-[url('./public/facilities-bg.png')]">
-      <div className=" pb-[60px] pt-[172px] mx-auto flex h-72 max-w-[76rem]">
+    <header className=" h-[367px] bg-[url('/facilities-bg.png')]">
+      <div className=" mx-auto flex h-72 max-w-[76rem] pb-[60px] pt-[172px]">
         <h1 className=" mr-[545px] text-[58px] font-semibold leading-[72px] text-white">
           Conoce nuestro espacio
         </h1>
