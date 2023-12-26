85f0e35a6e29b5b3c1b54aff1a12474a52393cb8
miguel <miguelbranagan30@gmail.com>
2023-12-26 00:14:48 -0400

    adding bg to the contact page

--- Front/src/pages/Contact.jsx
+++ Front/src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import { ContactHeader } from "../components/ContactComponent.jsx/ContactHeader";
 import ContactForm from "../components/ContactComponent.jsx/ContactForm";
+import ContactInformation from "../components/ContactComponent.jsx/ContactInformation";
 
 export const Contact = () => {
   return (
     <div>
       <ContactHeader />
+      <ContactInformation />
       <ContactForm />
     </div>
   );
